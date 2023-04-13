8988acf87d46dc2036eec5e727c7d2d5e597e8a3
Vanshika Goel <vgoel3103@gmail.com>
2023-04-13 12:16:11 +0530

    Added changes

--- client/src/Common.js
+++ client/src/Common.js
@@ -1,3 +1,3 @@
 // const baseURL = 'http://localhost:8000';
-const baseURL = "https://server-production-6752.up.railway.app/"
+const baseURL = "https://server-production-6752.up.railway.app"
 export default baseURL;
