af45760edcf81d04516220c81c21cea9a1403106
Vanshika Goel <vgoel3103@gmail.com>
2023-04-26 01:55:36 +0530

    Added connectivity

--- client/src/Common.js
+++ client/src/Common.js
@@ -1,3 +1,3 @@
-const baseURL = 'http://localhost:8000';
-// const baseURL = "https://server.vanshikagoel11.repl.co"
+//const baseURL = 'http://localhost:8000';
+const baseURL = "https://bv-placed-full-project-production.up.railway.app/"
 export default baseURL;
