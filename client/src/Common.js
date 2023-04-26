78946c6cd7eb8b3d427f1049d6b10d096daa8e76
Vanshika Goel <91647992+vanshikagoel1@users.noreply.github.com>
2023-04-26 10:39:38 +0530

    Update Common.js

--- client/src/Common.js
+++ client/src/Common.js
@@ -1,3 +1,3 @@
 //const baseURL = 'http://localhost:8000';
-const baseURL = "https://bv-placed-full-project-production.up.railway.app"
+const baseURL = "http://bv-placed-full-project-production.up.railway.app"
 export default baseURL;
